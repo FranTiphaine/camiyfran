ff9e47016d1f26e414139f59c34a3bb8db9628d7
Francisco Tiphaine <franciscotiphaine@gmail.com>
2022-06-17 18:32:30 -0300

    Se agrega ejercicio 2 javascript

--- Modulo3-Unidad4/js/script.js
+++ Modulo3-Unidad4/js/script.js
@@ -5,3 +5,11 @@ function cambiarFondo(color) {
     document.querySelector('body').style.background = color;
 }
 
+/* Ejercicio 2 */
+
+let text = document.getElementById('text');
+let count = document.getElementById('count');
+
+text.addEventListener('keyup', function (){
+    count.innerText = text.value.length;
+})
